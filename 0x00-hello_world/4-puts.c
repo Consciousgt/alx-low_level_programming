f3241ca8484f6d3515f5ce8ab7c7476ddf4a38a8
consciousgt <consciousgt@gmail.com>
2022-09-08 18:32:06 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <studio.h>
 
 /**
- * main - Entry point
+ * main -Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Sucess)
  */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
