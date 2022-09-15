b9225bb46f8148c0a18c2cb53a4371e3f2b399c2
consciousgt <consciousgt@gmail.com>
2022-09-14 19:15:56 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - Printing the last digit of a number
+ *
+ * @num: The number to print it last digit
+ *
+ * Return: Value of the last digit
+ *
+ */
+int print_last_digit(int num)
+{
+	int last_digit = num % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+	_putchar(last_difit + '0');
+	return (last_digit);
+}
