0bfaf4146afef39f5d1fc370ca3512236722e059
consciousgt <consciousgt@gmail.com>
2022-09-20 21:43:49 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,32 +1,35 @@
 #include "main.h"
 /**
- * _atoi - a function that converts string to integer
+ * _atoi - convert a string to an integer
  *
- * @s: An input string
+ * @s: the string to use
  *
- * Return: integer from conversion
+ * Return: integer
  *
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	unsigned int total = 0;
-	char null_flag = 0;
+	int counter = 0;
+	int i = 0;
+	unsigned int result = 0;
+	int sign = 0;
 
-	while (*s)
+	while (s[counter] != '\0')
 	{
-		if (*s == '_')
-			sign *= -1;
-		if (*s >= '0' && *s <= '9')
+		if (s[counter] == '-')
+			sign++;
+		if (s[counter] >= 48 && s[counter] <= 57)
 		{
-			null_flag = 1;
-			total = total * 10 + *s - '0';
+			i++;
+			result = result * 10 + s[counter] - '0';
 		}
-		else if (null_flag)
+		if (i > 0 && (s[counter] < 48 || s[counter] > 57))
 			break;
-		s++;
+		counter++;
 	}
-	if (sign < 0)
-		total = (-total);
-	return (total);
+	if (i == 0)
+		return (0);
+	if (sign % 2 == 0)
+		return (result);
+	return (result * -1);
 }
