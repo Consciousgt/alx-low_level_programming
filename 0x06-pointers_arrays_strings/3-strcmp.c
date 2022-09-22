51c354b9441b33cb03c0011b2ee5384de331a734
consciousgt <consciousgt@gmail.com>
2022-09-21 20:14:47 -0700

    Task 4

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ *
+ * @str1: A pointer to a character that will be changed
+ *
+ * @str2: A pointer to a character that will also be changed/modified/updated
+ *
+ * Return: dest
+ */
+int _strcmp(char *str1, char *str2)
+
+{
+	char *str_one = str1;
+	char *str_two = str2;
+
+	while (*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
+	{
+		str_one++;
+		str_two++;
+	}
+	return (*str_one - *str_two);
+}
